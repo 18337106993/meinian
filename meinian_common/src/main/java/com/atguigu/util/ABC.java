61be36fe45d43ba457f480f6431933227d37653a
代树磊 <923685280@qq.com>
2022-02-27 17:49:35 +0800

    美年旅游代码

---
+++ meinian_common/src/main/java/com/atguigu/util/ABC.java
@@ -0,0 +1,8 @@
+package com.atguigu.util;
+
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("11111111111111111mast");
+        System.out.println("11111111111111111dev");
+    }
+}
